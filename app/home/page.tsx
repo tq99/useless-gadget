224b383a9585c7b140c19dd25a21bdb1c95ffa60
aaqil <kidobop@gmail.com>
2024-10-25 17:49:27 +0530

    added shadcn ui

---
+++ app/home/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button"
+const HomePage = ()=>{
+
+    return (
+        <div>
+            <div>
+
+                Helooo
+                <Button/>
+            </div>
+        </div>
+    )
+
+}
+
+
+export default HomePage
